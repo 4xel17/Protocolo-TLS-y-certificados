65b6cfad9a948e8ffff63d29c28a1e95f96f74e4
4xel17 <axelbequ@unisabana.edu.co>
2025-11-18 19:04:37 -0500

    Update and rename Prueba1 to Prueba1.ino

---
+++ codigos_pruebas/Prueba1.ino
@@ -0,0 +1,65 @@
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <PubSubClient.h>
+
+// Credenciales WiFi
+const char* ssid = "iPhone";
+const char* password = "123456789";
+
+// Broker MQTT seguro
+const char* mqtt_server = "broker.hivemq.com";
+const int mqtt_port = 8883;   // <--- Puerto seguro TLS
+
+WiFiClientSecure espClient;
+PubSubClient client(espClient);
+
+void setup_wifi() {
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nWiFi conectado");
+}
+
+void callback(char* topic, byte* message, unsigned int length) {
+  Serial.print("Mensaje recibido: ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)message[i]);
+  }
+  Serial.println();
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Conectando a MQTT...");
+    if (client.connect("ESP32TestTLS")) {
+      Serial.println("Conectado!");
+      client.subscribe("prueba/esp32");
+    } else {
+      Serial.print("Fallo, rc=");
+      Serial.print(client.state());
+      delay(2000);
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  setup_wifi();
+
+  // NO VALIDAMOS CERTIFICADO TODAVÍA
+  espClient.setInsecure();   // <--- IMPORTANTE
+
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+  client.publish("prueba/esp32", "Mensaje desde ESP32 sin cert");
+  delay(3000);
+}
